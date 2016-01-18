04d20bb29ab46bfc3c6479d5c8f260be920f1750
Ronny Løvtangen <ronny@lovtangen.com>
2016-01-18 08:18:21 +0100

    Grails 3 support

--- src/main/resources/extensions/sdkman-switch.sh
+++ src/main/resources/extensions/sdkman-switch.sh
@@ -2,10 +2,20 @@
 
 function __sdk_switch {
 	CANDIDATE="$1"
-	if [[ -z "$1" ]]; then
+	if [[ -z "$CANDIDATE" ]]; then
 		if [[ -f application.properties ]]; then
 			$(IFS=" ")
 			POSSIBLE_CANDIDATES=( $(cat application.properties | grep "app\..*\.version" | sed 's/app.//' | sed 's/.version.*//g') )
+			for c in "${POSSIBLE_CANDIDATES[@]}"
+			do
+				if [[ "$SDKMAN_CANDIDATES_CSV" == *"${c}"* ]]; then
+					CANDIDATE="${c}"
+				fi
+			done
+		fi
+		if [[ -z "$CANDIDATE" && -f gradle.properties ]]; then
+			$(IFS=" ")
+			POSSIBLE_CANDIDATES=( $(cat gradle.properties |grep ".*Version" |sed 's/Version.*//g') )
 			for c in "${POSSIBLE_CANDIDATES[@]}"
 			do
 				if [[ "$SDKMAN_CANDIDATES_CSV" == *"${c}"* ]]; then
@@ -18,12 +28,17 @@ function __sdk_switch {
 	if [[ -z "$CANDIDATE" ]]; then
 		echo "No candidate provided."
 	else
+		VERSION=""
 		if [[ -f application.properties && -d "$CANDIDATE-app" ]]; then
 			VERSION=$(cat application.properties | grep "app.$CANDIDATE.version" | sed "s!app.$CANDIDATE.version=!!g")
+		elif [[ -f gradle.properties && -d "$CANDIDATE-app" ]]; then
+			VERSION=$(cat gradle.properties | grep ${CANDIDATE}Version | cut -d '=' -f2)
+		fi
+		if [[ -z "$VERSION" ]]; then
+			echo "Stop! This is not a valid $CANDIDATE project!"
+		else
 			echo "Switching to $CANDIDATE $VERSION ..."
 			__sdk_use "$CANDIDATE" "$VERSION"
-		else
-			echo "Stop! This is not a valid $CANDIDATE project!"
 		fi
 	fi
 }
